fd01743e9f59eb5c97d744a057f5f5279bc2fe0b
Dan <dstein64@users.noreply.github.com>
2025-05-12 10:13:58 -0400

    Change poll
    The existing poll page is returning a 403 status code when fetched from
    GitHub Actions.

--- tests.js
+++ tests.js
@@ -9,7 +9,7 @@ const {JSDOM} = jsdom;
 
 const {extract} = require(__dirname + '/src/extract.js');
 
-const url = 'https://news.ycombinator.com/item?id=30055031';
+const url = 'https://news.ycombinator.com/item?id=42836273';
 https.get(url, resp => {
     assert.equal(resp.statusCode, 200);
     let data = '';
@@ -21,15 +21,13 @@ https.get(url, resp => {
         const extracted = extract(dom.window.document);
         assert.notEqual(extracted, null);
         const {items, tbody} = extracted;
-        assert.equal(items.length, 3);
+        assert.equal(items.length, 2);
         const sortedItems = [...items].sort((a, b) => a.name.localeCompare(b.name));
-        assert.equal(sortedItems[0].name, 'Linux');
-        assert(sortedItems[0].score >= 247);
-        assert.equal(sortedItems[1].name, 'MacOS');
-        assert(sortedItems[1].score >= 172);
-        assert.equal(sortedItems[2].name, 'Windows');
-        assert(sortedItems[2].score >= 115);
-        assert.equal(tbody.children.length, 9);
+        assert.equal(sortedItems[0].name, 'Rust');
+        assert(sortedItems[0].score >= 7);
+        assert.equal(sortedItems[1].name, 'Zig');
+        assert(sortedItems[1].score >= 6);
+        assert.equal(tbody.childElementCount, 6);
     });
 }).on('error', err => {
     assert.fail(err.message);
